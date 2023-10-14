84aa51c77baef2808049f427381e5a56f4b1f771
GoreC1 <hazich322@gmail.com>
2023-10-14 17:40:05 +0500

    lab2

--- index.php
+++ index.php
@@ -1,46 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Page Title</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
-</head>
-<body>
 <?php
-include 'config.php';
+  $сontent = file_get_contents(__DIR__ . '/Лаб_Парсер.htm');
 
+  $convertedContent = iconv('Windows-1251', 'UTF-8', $сontent);
 
-// $request = $_SERVER['REQUEST_URI'];
+  $pattern = '/<td bgcolor="#ffffff">(.*?)<!--End of Main Menu-->/s';
 
-// switch ($request) {
-//     case '/' :
-//         require __DIR__ . '/index.php';
-//         break;
-//     case '' :
-//         require __DIR__ . '/index.php';
-//         break;    
-//     case '/passengers' :
-//         require __DIR__ . '/passengers.php';
-//         break;
-//     case '/soldTickets' :
-//         require __DIR__ . '/soldTickets.php';
-//         break;
-//     case '/flights' :
-//         require __DIR__ . '/flights.php';
-//         break;    
-//     default:
-//         http_response_code(404);
-//         require __DIR__ . '/views/404.php';
-//         break;
-// }
-?>    
-<a href="passengers.php">Passengers</a>
-<a href="flights.php">Flights</a>
-<a href="soldTickets.php">Sold tickets</a>
-<a href="queries.php">Queries</a>
+  preg_match($pattern, $convertedContent, $matches);
 
-</body>
-</html>
+  $leftMenu = $matches[0];
+  
+  $pattern = '/<span class="navText2">(.*?)<\/span>/s';
+
+  preg_match_all($pattern, $leftMenu, $menuItems);
+
+  foreach ($menuItems[0] as $item) {
+    echo "<div>$item</div>";
+  }
+
+  $pattern = '/\b[A-ZА-ЯЁ]+\b/';
+  $count = preg_match_all($pattern, join("", $menuItems[0]), $count);
+  echo "<h2>Количество слов, напечатанных заглавными буквами: $count</h2>";
+?>
