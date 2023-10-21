5c7bd4130ee3804b39a096d2180d5ea13dd050b0
GoreC1 <hazich322@gmail.com>
2023-10-21 15:49:26 +0500

    lab2 done

--- index.php
+++ index.php
@@ -1,23 +1,32 @@
 <?php
   $сontent = file_get_contents(__DIR__ . '/Лаб_Парсер.htm');
-
+  
+  
   $convertedContent = iconv('Windows-1251', 'UTF-8', $сontent);
-
+  
+  $convertedContent = str_replace('&nbsp;', ' ', $convertedContent);
+  
   $pattern = '/<td bgcolor="#ffffff">(.*?)<!--End of Main Menu-->/s';
 
   preg_match($pattern, $convertedContent, $matches);
 
+
   $leftMenu = $matches[0];
   
-  $pattern = '/<span class="navText2">(.*?)<\/span>/s';
+  $pattern = '/(?<=<span class="navText2">)(.*?)(?=<\/span>)/s';
 
   preg_match_all($pattern, $leftMenu, $menuItems);
 
+  $fd = fopen("parsed.txt", "w");
+
   foreach ($menuItems[0] as $item) {
     echo "<div>$item</div>";
+    fwrite($fd, "$item\n");
   }
 
-  $pattern = '/\b[A-ZА-ЯЁ]+\b/';
+  $pattern = '/\b[A-ZА-Я]*\b/u';
   $count = preg_match_all($pattern, join("", $menuItems[0]), $count);
   echo "<h2>Количество слов, напечатанных заглавными буквами: $count</h2>";
+  fwrite($fd, "Количество слов, напечатанных заглавными буквами: $count\n");
+
 ?>
