1e3581a2d36808e6442c45fe2016d8a6a1c4995d
GoreC1 <hazich322@gmail.com>
2023-11-01 10:55:42 +0500

    labTags done

--- index.php
+++ index.php
@@ -1,2 +1,34 @@
 <?php
+
+$json = json_decode(file_get_contents("./keyWords.txt"), true);
+
+$max_count = 0;
+$min_count = 0;
+
+foreach ($json as $key){
+  
+  if ($key["views"] > $max_count){
+    $max_count = $key["views"];
+  }
+
+  if ($key["views"] < $min_count){
+    $min_count = $key["views"];
+  }
+}
+
+$max_font_size = 45;
+$min_font_size = 10;
+
+foreach ($json as $key => $val) {
+    
+    $font_size = ($val["views"] - $min_count) / ($max_count - $min_count)
+                 * ($max_font_size - $min_font_size) + $min_font_size;
+    ?>
+    <a href="./pages/<?php echo $key ?>.php">
+    <div style="font-size:<?php echo $font_size?>px">
+      <?php echo $val["words"];?> </div>
+    </a>
+    <hr>  
+<?php
+}
 ?>
---
+++ pages/2.php
@@ -0,0 +1,30 @@
+<div>
+    <a href="../index.php"> На главную
+    </a> 
+</div>
+
+<?php
+
+$json = json_decode(file_get_contents("../keyWords.txt"), true);
+
+$json["2"]["views"]++;
+
+file_put_contents("../keyWords.txt", json_encode($json, JSON_PRETTY_PRINT));
+
+$lines = file("../Text/2.txt");
+
+$linesWithCorrectEncode = array_map(
+    function($line) {
+    return iconv('Windows-1251', 'UTF-8', $line);
+    },
+    $lines
+);
+
+foreach ($linesWithCorrectEncode as $line){
+    ?> <div>
+        <?php echo $line; ?>
+        
+    </div> 
+<?php
+}
+?>
